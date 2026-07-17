57d7701c0a66dfb00d2c43d629fef155b1d3a109
san40-u5an40 <alexandr.dev2011@gmail.com>
2026-07-17 14:58:17 +0300

    Изменена сортировка в yazi

--- home-manager/programs/modules/yazi.nix
+++ home-manager/programs/modules/yazi.nix
@@ -5,7 +5,7 @@
       mgr = {
         show_hidden = true;
         show_symlink = true;
-        sort_by = "alphabetical";
+        sort_by = "natural";
         sort_sensitive = false;
         sort_dir_first = true;
         ratio = [ 1 4 3 ];
